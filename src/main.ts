f9f55c022603b3fc945f3c8438daf7b3c27f3bd8
Dmytro Mezhenskyi <dmytro@decodedfrontend.io>
2022-08-26 20:46:25 +0200

    Include angular material

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
 import { environment } from './environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent)
+bootstrapApplication(AppComponent, {
+    providers: [importProvidersFrom(BrowserAnimationsModule)]
+})
   .catch(err => console.error(err));
